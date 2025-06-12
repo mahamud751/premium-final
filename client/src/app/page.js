cc51f9b92cff004505f623953adb19bc7228414d
Mahamud Pino <mahamudpino24678@gmail.com>
2025-06-12 09:47:53 +0600

    add home

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Home_V1 from "./(home)/home-v1/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
@@ -8,7 +9,17 @@ export const metadata = {
 export default function MainRoot() {
   return (
     <Wrapper>
-      <Home_V1 />
+      <div className="flex justify-center items-center">
+        <Image
+          src={"https://i.ibb.co/RpzCZgw9/un.jpg"}
+          alt="Banner Image"
+          width={1920}
+          height={1080}
+          className="w-1/2"
+        />
+      </div>
+
+      {/* <Home_V1 /> */}
     </Wrapper>
   );
 }
